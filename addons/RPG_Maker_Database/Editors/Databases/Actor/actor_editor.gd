57af31bc5c68c5d05248131dab2184d8f16591a1
JamesMWestbrook <james.michael.westbrook@gmail.com>
2025-01-18 18:22:58 -0600

    Save load basic info

--- addons/RPG_Maker_Database/Editors/Databases/Actor/actor_editor.gd
+++ addons/RPG_Maker_Database/Editors/Databases/Actor/actor_editor.gd
@@ -21,11 +21,12 @@ func _ready() -> void:
 	save_button.pressed.connect(_save_json)
 	name_edit.text_changed.connect(_update_name)
 	nickname_edit.text_changed.connect(_update_nickname)
-	initial_level_edit.changed.connect(_start_level)
-	max_level_edit.changed.connect(_max_level)
+	initial_level_edit.value_changed.connect(_start_level)
+	max_level_edit.value_changed.connect(_max_level)
+	profile_edit.text_changed.connect(_profile)
 	
+	_load_actor(0)
 	
-	actor_container.get_child(0).pressed.emit()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -44,18 +45,61 @@ func _check_json():
 			var new_button:Button = Button.new()
 			actor_container.add_child(new_button)
 			
-			new_button.text = "Name Here"
+			new_button.text = "Actor " + str(index)
 			new_button.pressed.connect(_load_actor.bind(index))
 			actors.append({})
 			index += 1
+	
 		
 func _load_json(file:FileAccess):
-	pass
+	var save_data:Dictionary
+	var json_string:String = file.get_as_text()
+	save_data = JSON.parse_string(json_string)
+	actors = save_data["actors"]
+	var index:int = 0
+	for actor:Dictionary in actors:
+		var new_button:Button = Button.new()
+		actor_container.add_child(new_button)
+		
+		if actor.has("name"):
+			new_button.text = actor["name"]
+		else:
+			new_button.text = "Actor " + str(index)
+		
+		new_button.pressed.connect(_load_actor.bind(index))
+		index += 1
 	
 func _load_actor(index:int):
 	print("Actor " + str(index) + " selected")
 	cur_actor_index = index
-
+	var actor:Dictionary = actors[cur_actor_index]
+	if actor.has("name"):
+			name_edit.text = actor["name"]
+	else:
+			name_edit.text = "Actor " + str(index)
+			_update_name("Actor " + str(index))
+		
+	if actor.has("nickname"):
+			nickname_edit.text= actor["nickname"]
+	else:
+		nickname_edit.text = ""
+		_update_nickname("")
+	if actor.has("start_level"):
+		initial_level_edit.value = actor["start_level"]
+	else:
+		initial_level_edit.value = 1
+		_start_level(1)
+	if actor.has("max_level"):
+		max_level_edit.value = actor["max_level"]
+	else:
+		max_level_edit.value = 99
+		_max_level(99)
+	if actor.has("profile"):
+		profile_edit.text = actor["profile"]
+	else:
+		profile_edit.text = ""
+		_profile()
+			
 func _update_name(text):
 	actors[cur_actor_index]["name"] = text
 	
@@ -67,6 +111,9 @@ func _start_level(index):
 	
 func _max_level(index):
 	actors[cur_actor_index]["max_level"] = index
+func _profile():
+	actors[cur_actor_index]["profile"] = profile_edit.text
+	
 	
 func _save_json():
 	var save_data:Dictionary = {

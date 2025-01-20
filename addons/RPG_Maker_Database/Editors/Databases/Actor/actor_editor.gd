74b09fa7a90097fe8687f73aa39e5df279e88dd6
JamesMWestbrook <james.michael.westbrook@gmail.com>
2025-01-19 20:55:55 -0600

    Resizable and did actor screen

--- addons/RPG_Maker_Database/Editors/Databases/Actor/actor_editor.gd
+++ addons/RPG_Maker_Database/Editors/Databases/Actor/actor_editor.gd
@@ -14,26 +14,23 @@ class_name ActorEditor
 		battler_sprite.texture = null
 		
 #region Onreadys
-@export var actor_container:VBoxContainer = $ScrollContainer/ActorsVBox
-@export var name_edit:LineEdit = $NameLabel/NameEdit
-@export var nickname_edit: LineEdit = $NicknameLabel/NicknameEdit
-@export var initial_level_edit: SpinBox = $InitialLevelLabel/InitialLevelEdit
-@export var max_level_edit: SpinBox = $MaxLevelLabel/MaxLevelEdit
-@export var profile_edit: TextEdit = $ProfileLabel/ProfileEdit
+@onready var actor_container:VBoxContainer = $BoxContainer/Column1/ScrollContainer/ActorsVBox
+@onready var name_edit:LineEdit = $BoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/Column2/HBoxContainer2/NameEdit
+@onready var nickname_edit: LineEdit =$BoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/Column2/HBoxContainer2/NicknameEdit
+@onready var initial_level_edit: SpinBox = $BoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/Column3/InitialLevelEdit
+@onready var max_level_edit: SpinBox = $BoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/Column3/MaxLevelEdit
+@onready var profile_edit: TextEdit = $BoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/Column2/ProfileEdit
 
-@export var face_file_dialog: FileDialog = $FaceFileDialog
-@export var face_sprite: Sprite2D = $FaceButton/FaceSprite
-@export var face_button: Button = $FaceButton
-@export var clear_face_button: Button = $FaceButton/ClearFaceButton
-@export var face_index_spinbox: SpinBox = $FaceButton/FaceIndexSpinbox
+@onready var face_sprite: Sprite2D = $BoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/Column2/HBoxContainer3/FaceButton/FaceSprite
+@onready var face_button: Button = $BoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/Column2/HBoxContainer3/FaceButton
+@onready var face_index_spinbox: SpinBox = $BoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/Column2/HBoxContainer4/FaceIndexSpinbox
 
-@export var sprite_button: Button = $SpriteButton
-@export var walking_sprite: Sprite2D = $SpriteButton/WalkingSprite
-@export var sprite_index_spinbox: SpinBox = $SpriteButton/SpriteIndexSpinbox
-@export var sprite_file_dialog: FileDialog = $SpriteFileDialog
+@onready var sprite_button: Button = $BoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/Column2/HBoxContainer3/SpriteButton
+@onready var walking_sprite: Sprite2D = $BoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/Column2/HBoxContainer3/SpriteButton/WalkingSprite
+@onready var sprite_index_spinbox: SpinBox = $BoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/Column2/HBoxContainer4/SpriteIndexSpinbox
 
-@export var battler_button: Button = $BattlerButton
-@export var battler_sprite: Sprite2D = $BattlerButton/BattlerSprite
+@onready var battler_button: Button = $BoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/Column2/HBoxContainer3/BattlerButton
+@onready var battler_sprite: Sprite2D = $BoxContainer/ScrollContainer/VBoxContainer/HBoxContainer/Column2/HBoxContainer3/BattlerButton/BattlerSprite
 
 #endregion
 

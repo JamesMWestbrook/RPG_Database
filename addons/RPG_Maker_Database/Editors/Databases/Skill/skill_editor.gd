b21bd20ef19f15a653e45676aa6be10cd961e58c
JamesMWestbrook <james.michael.westbrook@gmail.com>
2025-01-24 13:46:40 -0600

    Transferring to windows

--- addons/RPG_Maker_Database/Editors/Databases/Skill/skill_editor.gd
+++ addons/RPG_Maker_Database/Editors/Databases/Skill/skill_editor.gd
@@ -1,11 +1,60 @@
 extends Control
 
+#region onreadys
+@onready var name_edit: LineEdit = $"ScrollContainer/BoxContainer/2nd Column/NameEdit"
+@onready var skills_box: VBoxContainer = $"ScrollContainer/BoxContainer/1st Column/ScrollContainer/SkillsBox"
 
+#endregion
+
+
+
+var cur_skill_index:int
+var skill_count:int
+var skills=[]
+const JSON_SAVE_PATH = "res://data/skills.json"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
+	_check_json()
+	var button:Button = skills_box.get_child(0)
+	button.button_down.emit()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _check_json():
+	if FileAccess.file_exists(JSON_SAVE_PATH):
+		var file = FileAccess.open(JSON_SAVE_PATH,FileAccess.READ)
+		_load_json(file)
+	else:
+		skill_count = 5
+		for i in skill_count:
+			var new_button:Button = Button.new()
+			skills_box.add_child(new_button)
+			
+			new_button.text = "Skill " + str(i)
+			skills.append({})
+			new_button.button_down.connect(_load_class.bind(i))
+			
+func _load_json(index:int):
+	pass
+	
+func _load_class(index:int):
+	cur_skill_index = index
+	var skill = skills[index]
+	
+	if skill.has("name"):
+		name_edit.text = skill.name
+	else:
+		name_edit.text = "Skill " + str(index)
+		_update_name(name_edit.text)
+	_check_skill(skills[cur_skill_index])
+	_load_skill_stats(skills[cur_skill_index])
+	
+func _check_skill(skill:Dictionary):
+	pass
+func _load_skill_stats(skill:Dictionary):
+	pass
+func _update_name(text:String):
+	skills[cur_skill_index].name = text
+	skills_box.get_child(cur_skill_index).text = text
